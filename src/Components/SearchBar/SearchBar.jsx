fc61bdbb02a42d7369ff770e3a2c65ef2e530ff7
Victor Gomez <victorgomezdev87@gmail.com>
2025-07-23 12:28:06 -0300

    ups falto

---
+++ src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,16 @@
+import InputForm from '../Inputs/InputForm/InputForm';
+import './searchBar.css';
+import { FaSearch } from "react-icons/fa";
+
+const SearchBar = () => {
+	return (<div className='search-bar'>
+		<div className="search-bar-container">
+			<div className="search-bar-title">Encuentra lo que buscas aquí!</div>
+			<div className="search-bar-input-bar">
+				<InputForm placeholder={"Buscar..."} icon={<FaSearch />}></InputForm>
+			</div>
+		</div>
+	</div>);
+}
+
+export default SearchBar;
