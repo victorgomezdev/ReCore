9ea63a7b233a854cfa068f262804cc027b5cd44d
VictorG <victorgomezdev87@gmail.com>
2025-07-25 07:08:16 -0300

    editando categorias

--- src/ThemeContext.jsx
+++ src/ThemeContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState, useEffect } from "react";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState('dark');
 
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
